e887ff47e534cde74e247b3ca49ea98938974d7b
surfing-coder <142689071+queso-coder@users.noreply.github.com>
2024-06-28 12:08:38 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDawwSN-QVv6GRw7pQkliE_Yz1qxGkAb5Q",
+  authDomain: "mycitybot-ommy.firebaseapp.com",
+  projectId: "mycitybot-ommy",
+  storageBucket: "mycitybot-ommy.appspot.com",
+  messagingSenderId: "883432980452",
+  appId: "1:883432980452:web:f38988b5818f875da591f1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
